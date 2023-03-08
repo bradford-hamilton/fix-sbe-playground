6cf11aec1807cb5a5293137d280c14d894e5f040
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2023-03-07 17:51:38 -0700

    main ready to go

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "fix_sbe_playground/TradeData.h"
+#include "fix_sbe_playground/MessageHeader.h"
+
+using namespace fix::sbe::playground;
+
+int main() {
+  // TradeData td;
+  // MessageHeader hdr;
+
+  return 0;
+}
